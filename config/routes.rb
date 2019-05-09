dcc49b6880ca28a3e43fb5614523b045fa36bd97
seyepet <seyepet@gmail.com>
2019-05-09 10:47:20 +0000

    h

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  
+ devise_scope :user do
+  root to: "devise/registrations#new"
+ end
+ 
+ 
   resources :posts
   devise_for :users
   resources :people
-  root 'people#index'
+  get 'people/index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
