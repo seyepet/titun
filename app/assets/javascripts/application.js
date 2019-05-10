bc254ae5f70668315699fb1bc214747c33bd6aba
seyepet <seyepet@gmail.com>
2019-05-10 14:03:39 +0000

    m

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+<script type="text/javascript">     
+    $(document).ready(function() {
+     // executes when HTML-Document is loaded and DOM is ready
+    console.log("document is ready");
+      
+    
+      $( ".card" ).hover(
+      function() {
+        $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+      }, function() {
+        $(this).removeClass('shadow-lg');
+      }
+    );
+      
+    // document ready  
+    });
+    
+</script>    
