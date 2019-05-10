8d7f90ecde9e183ae4f03bb2401035b58e5bc2b8
seyepet <seyepet@gmail.com>
2019-05-10 15:11:12 +0000

    li

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,21 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-<script type="text/javascript">     
-    $(document).ready(function() {
-     // executes when HTML-Document is loaded and DOM is ready
-    console.log("document is ready");
-      
-    
-      $( ".card" ).hover(
-      function() {
-        $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
-      }, function() {
-        $(this).removeClass('shadow-lg');
-      }
-    );
-      
-    // document ready  
-    });
-    
-</script>    
