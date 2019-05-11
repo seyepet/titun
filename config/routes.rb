aea0d6087bf960a7c0f5bfc8a9cd7ffa67b48458
seyepet <seyepet@gmail.com>
2019-05-11 01:32:31 +0000

    kio

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   
+ devise_for :users
+ 
+ 
  devise_scope :user do
   root to: "devise/registrations#new"
  end
  
  
   resources :posts
-  devise_for :users
+  
   resources :people
  
   
