a06443c9b634b9a3b1265ae524d3734efdeb0987
seyepet <seyepet@gmail.com>
2019-05-24 17:53:45 +0000

    kijuiuo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   
   config.action_mailer_default_url_options = { host: 'https://akobe.org' }
   Rails.application.routes.default_url_options[:host] = 'https://akobe.org'
-  config.action_mailer.default_url_options = { :host => 'new_app_name.herokuapp.com' }  
+  config.action_mailer.default_url_options = { :host => 'https://akobe.org' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'new_app_name.herokuapp.com',
+  domain:               'https://akobe.org',
   user_name:            ENV["GMAIL_EMAIL"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
