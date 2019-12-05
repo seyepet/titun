07b2d3a290f93386cd6d11d6065b0e57e90f5f7f
seyepet <seyepet@gmail.com>
2019-12-05 07:30:22 +0000

    uioi

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,5 @@ class Category < ApplicationRecord
     has_many :product_categories, :dependent => :destroy
     has_many :products, through: :product_categories, :dependent => :destroy
     
-    # has_many :products, through: :product_categories
-    # has_many :product_categories
-    
+  
 end
