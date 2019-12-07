f31edcec1379214faaee136eb6cb68483807d70f
seyepet <seyepet@gmail.com>
2019-12-07 23:16:15 +0000

    yuty

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,44 +2,15 @@ class OrderItemsController < ApplicationController
 
   def index
     @items = current_cart.order.items
-    # OrderMailer.order_items(@order_items).deliver_now
-  
-    # UserMailer.user_mailer(@user).deliver
   end
-  
-  # OrderMailer.confirm(order_item)
-
-
-    # def confirm(order_item)
-    # @subject = 'Company Order Confirmation'
-    # @body["order"] = order
-    # @recipients = order.email
-    # @bcc = 'orders@… '
-    # @from = 'orders@…  '
-    # @sent_on = Time.now
-    # @headers["Organization"] = "Company"
-    # end
 
   def create
-    
     current_cart.add_item(
       product_id: params[:product_id],
       quantity: params[:quantity]
     )
 
     redirect_to cart_path
-  end
-  # def show
-  #   @order_items = current_order.order_items
-  #   @shipping = params[:shipping]
-
-  #   OrderMailer.order_send(@order, @client).deliver
-  #   flash[:notice] = "Order has been sent."
-  #   redirect_to cart_path(@order.id)
-  # end
-
-  def order_subtotal
-   @order_items.sum(:total_price)
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -42,17 +42,17 @@ Rails.application.routes.draw do
   
 #   get '/search' => 'products#search', :as => 'search_page'
 
-#   root to: 'categories#index'
+  # root to: 'categories#index'
 
-#   resources :categories, only: [:index] do
-#     resources :products, only: [:index]
-#   end
+  resources :categories, only: [:index] do
+    resources :products, only: [:index]
+  end
 
-#   get '/cart', to: 'order_items#index'
-#   resources :order_items, path: '/cart/items'
+  get '/cart', to: 'order_items#index'
+  resources :order_items, path: '/cart/items'
   
-# get '/cart/checkout', to: 'orders#new', as: :checkout
-# patch '/cart/checkout', to: 'orders#create'
+get '/cart/checkout', to: 'orders#new', as: :checkout
+patch '/cart/checkout', to: 'orders#create'
   
 # match "/settings_redirect" => "settings#redirect", :via => "get"
 #   get :send_order_mail, to: 'order_items#send_order_mail', as: :send_order_mail
