639ac4f17710982d7cb71e834e6aef6e4124fc83
seyepet <seyepet@gmail.com>
2019-12-10 13:23:13 +0000

    lolol

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,20 @@
 Rails.application.routes.draw do
-  resources :internetsecurities
-  resources :bags
-  resources :dslrcameras
-  resources :newhplaptops
-  resources :itelphones
-  resources :intelxphones
-  resources :infinixphones
-  resources :technophones
+  
   # resources :retrofits
-  resources :hprrlaptops
-  resources :hprlaptops
-  resources :factoryrecertifiedacerlaptops
-  resources :factoryrecertifieddelllaptops
-  resources :themastercares
-  resources :schools
-  get 'home/index'
-  get 'home/add'
-  resources :rofimachines
+  
+  # get 'home/index'
+  # get 'home/add'
  
-  get 'home/ronis'
-  get 'people/shop'
-  get 'home/marketplacehome'
-  get 'people/ticker'
-  resources :home
+  
+  # resources :home
 #   resources :solarworks
-  resources :tonycycles
-  resources :adeapartments
-  resources :dreamscomputers
-  resources :marketplaces
+  
   resources :categories
   resources :posts
   devise_for :users
-  resources :people
-  root 'people#index'
-  get 'home/index' 
+  
+  # get 'people#index'
+  # get 'home/index' 
   get 'home/contact_form' 
   post 'home/contact_form' 
   get 'home/contact_form' => 'home#index'
@@ -42,7 +23,7 @@ Rails.application.routes.draw do
   
 #   get '/search' => 'products#search', :as => 'search_page'
 
-  # root to: 'categories#index'
+  root to: 'categories#index'
 
   resources :categories, only: [:index] do
     resources :products, only: [:index]
