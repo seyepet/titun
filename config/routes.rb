f883b7e4f3a455604ad3d315d9072b8e6f42fb45
seyepet <seyepet@gmail.com>
2019-12-11 17:21:39 +0000

    632

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # resources :home
 #   resources :solarworks
   
-  resources :categories
+  # resources :categories
   resources :posts
   devise_for :users
   
@@ -17,11 +17,10 @@ Rails.application.routes.draw do
   # get 'home/index' 
   get 'home/contact_form' 
   post 'home/contact_form' 
-  get 'home/contact_form' => 'home#index'
-  get 'contacts/new' => 'dreamscomputers#show'
+  # get 'home/contact_form' => 'home#index'
+  # get 'contacts/new' => 'dreamscomputers#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-#   get '/search' => 'products#search', :as => 'search_page'
+   get '/search' => 'products#search', :as => 'search_page'
 
   root to: 'categories#index'
 
@@ -39,12 +38,12 @@ patch '/cart/checkout', to: 'orders#create'
 #   get :send_order_mail, to: 'order_items#send_order_mail', as: :send_order_mail
 #   resources :orders
   # get '/search' => 'hprlaptops#search', :as => 'search_page' 
-  get '/search' => 'retrofits#search', :as => 'search_page'  
+  # get '/search' => 'retrofits#search', :as => 'search_page'  
  
-  resources :retrofits do
-    get '/search' => 'retrofits#search', :as => 'search_page'
-  end
-  resources :solarworks do
-    get '/search' => 'solarworks#search', :as => 'search_page'
-  end
+  # resources :retrofits do
+    # get '/search' => 'retrofits#search', :as => 'search_page'
+  # end
+  # resources :solarworks do
+    # get '/search' => 'solarworks#search', :as => 'search_page'
+  # end
 end
