b21248891fc76ee929a5f6b2f150764c999e4c22
seyepet <seyepet@gmail.com>
2020-01-01 15:14:34 +0000

    klikuj

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,
-  domain: 'gmail.com',
-  user_name: ENV["GMAIL_EMAIL"],
+  domain: 'mail.google.com',
+  user_name: ENV["GMAIL_USERNAME"],
   password: ENV["GMAIL_PASSWORD"],
   authentication: 'plain',
   enable_starttls_auto: true }
