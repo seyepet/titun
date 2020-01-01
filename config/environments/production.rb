759de3f92c260446e277a8bd115f90459b687238
seyepet <seyepet@gmail.com>
2020-01-01 16:30:27 +0000

    uiuyh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,9 +104,8 @@ Rails.application.configure do
   
   
 
-  
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://www.akobe.org" }
-  config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
   config.action_mailer.default :charset => "utf-8"  
