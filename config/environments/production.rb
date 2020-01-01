680ebddb2640982728173e277ba9927cd51490c3
seyepet <seyepet@gmail.com>
2020-01-01 16:27:57 +0000

    koiou

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,23 +82,6 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-  
-  
- 
-  config.action_mailer.default_url_options = { host: "http://www.akobe.org" }
-  config.action_mailer.delivery_method = :smtp  
-  config.action_mailer.perform_deliveries = true  
-  config.action_mailer.raise_delivery_errors = false  
-  config.action_mailer.default :charset => "utf-8"  
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.sendgrid.net',
-  port:                 587,
-  domain:               'akobe.org',
-  user_name:            ENV['SENDGRID_USERNAME'],
-  password:             ENV['SENDGRID_PASSWORD'],
-  authentication:       :login,
-  enable_starttls_auto: true  
-  }
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
@@ -118,4 +101,24 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  
+
+  
+  config.action_mailer.default_url_options = { host: "http://www.akobe.org" }
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.sendgrid.net',
+  port:                 587,
+  domain:               'akobe.org',
+  user_name:            ENV['SENDGRID_USERNAME'],
+  password:             ENV['SENDGRID_PASSWORD'],
+  authentication:       :login,
+  enable_starttls_auto: true  
+  }
+
+
 end
