ee53b1e89c8046226737e14fa7678983d1f85b35
seyepet <seyepet@gmail.com>
2020-01-12 17:34:50 +0000

    opoiu

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,11 +1,6 @@
 
 
 Recaptcha.configure do |config|
-  config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
-  config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
-end
-
-Recaptcha.configure do |config|
   config.site_key  = ENV['RECAPTCHA_PUBLIC_KEY']
   config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
   # Uncomment the following line if you are using a proxy server:
